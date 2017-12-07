f96143541bf9c94ed787bf3ee55c4ffb21537939
YJsh <syj2014mail@163.com>
2017-12-07 21:10:37 +0800

    共享目录后台数据及接口

--- share/models.py
+++ share/models.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class FileNode(models.Model):
+    isDir = models.BooleanField(default=False)
+    name = models.FileField(max_length=30)
+    file = models.FileField(upload_to="share")
+    parent = models.ForeignKey("self", related_name='dir', null=True)
--- share/views.py
+++ share/views.py
@@ -3,8 +3,84 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import StreamingHttpResponse
-
+from django.core.exceptions import ObjectDoesNotExist
+from models import FileNode
 # Create your views here.
 
-def upload(request):
 
+def getTreeNodes(nodes, parent):
+    for node in nodes:
+        treeNode = {
+            "id": node.id,
+            "name": node.name,
+            "isParent": False,
+            "open": False,
+            "children": []
+        }
+        if node.isDir:
+            treeNode["isParent"] = True
+            getTreeNodes(node.dir.all(), treeNode)
+
+        parent["children"].append(treeNode)
+
+
+def getDirTree(request):
+    nodes = FileNode.objects.fileter(parent=None)
+    dirTree = [{
+        "id": -1,
+        "name": u"共享目录",
+        "isParent": True,
+        "open": True,
+        "children": []
+    }]
+
+    getTreeNodes(nodes, dirTree)
+
+
+def renameNode(request):
+    nodeId = request.POST.get("id", -1)
+    nodeName = request.POST.get("name", "")
+    try:
+        node = FileNode.objects.get(id=nodeId)
+        node.name = nodeName
+        node.save()
+    except ObjectDoesNotExist:
+        return
+
+
+def deleteTree(node):
+    if not node.isDir:
+        # 删除文件
+        pass
+
+    for node in node.dir.all():
+        deleteTree(node)
+
+
+def deleteNode(request):
+    nodeId = request.POST.get("id", -1)
+    try:
+        node = FileNode.objects.get(id=nodeId)
+        deleteTree(node)
+    except ObjectDoesNotExist:
+        return
+
+
+def addDir(request):
+    parentId = request.POST.get("parentId", -1)
+    nodeName = request.POST.get("name", "")
+    parent = None
+    try:
+        parent = FileNode.objects.get(id=parentId)
+    except ObjectDoesNotExist:
+        pass
+
+    node = FileNode(isDir=True, name=nodeName, parent=parent)
+    node.save()
+
+
+def addFile(request):
+    nodeName = ""
+    nodeFile = ""
+    node = FileNode(name=nodeName, file=nodeFile)
+    node.save()
