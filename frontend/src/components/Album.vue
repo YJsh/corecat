dede90116681acb8e570ee37fee1c4450daefef2
YJsh <syj2014mail@163.com>
2017-12-16 15:00:14 +0800

    相簿 Django app 及 Vue 组件

---
+++ frontend/src/components/Album.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <el-dialog :visible.sync="dialogVisible" size="tiny">
+      <img width="100%" :src="dialogImageUrl" alt="">
+    </el-dialog>
+    <el-upload
+      action="https://jsonplaceholder.typicode.com/posts/"
+      list-type="picture-card"
+      :on-preview="handlePictureCardPreview"
+      :on-remove="handleRemove">
+      <i class="el-icon-plus"></i>
+    </el-upload>
+  </div>
+</template>
+
+<script>
+  import Vue from "vue"
+  import { Dialog, Upload } from "element-ui"
+
+  Vue.use(Dialog);
+  Vue.use(Upload);
+
+  export default {
+    name: "album",
+    data() {
+      return {
+        dialogImageUrl: '',
+        dialogVisible: false
+      };
+    },
+    methods: {
+      handleRemove(file, fileList) {
+        console.log(file, fileList);
+      },
+      handlePictureCardPreview(file) {
+        this.dialogImageUrl = file.url;
+        this.dialogVisible = true;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
