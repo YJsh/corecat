a4d0c2cdf563273c70704476076241a558c9ac54
YJsh <syj2014mail@163.com>
2018-01-12 22:59:55 +0800

    增加侧边栏布局

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,12 +4,20 @@
   </div>
   <div v-else id="app">
     <nav-menu></nav-menu>
-    <router-view/>
+    <el-container>
+      <el-aside width="250px">Aside</el-aside>
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
   import NavMenu from './components/NavMenu';
+  import ElContainer from "element-ui/packages/container/src/main";
+  import ElMain from "element-ui/packages/main/src/main";
+  import ElAside from "element-ui/packages/aside/src/main";
 
   export default {
     name: 'app',
@@ -19,20 +27,30 @@
       },
     },
     components: {
+      ElAside,
+      ElMain,
+      ElContainer,
       NavMenu,
     }
   }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  padding-top: 50px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    padding-top: 50px;
+  }
 
-html,body,#loginApp {height: 100%;}
+  .el-aside {
+    background-color: #e6dbb6;
+  }
+
+  html,body,#loginApp {height: 100%;}
+
+  html,body,#app,.el-container {height: 100%;}
+
 </style>
